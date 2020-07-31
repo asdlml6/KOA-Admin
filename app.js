e04a060b6b334ef0469226c6aee22d3c1dcb9ec3
xiaojiujiu <3507440900@qq.com>
2020-07-31 17:29:40 +0800

    更新入口文件2.0

--- app.js
+++ app.js
@@ -6,10 +6,18 @@ const views=require('koa-views')
 const cors=require('koa2-cors')
 const svgCaptcha=require('svg-captcha')
 const path=require('path')
+const {sign}=require('jsonwebtoken')
+const secret='demo'
+const jwt=require('koa-jwt')({secret})
+const mongoose=require('mongoose')
 
 const app=new Koa()
 const router=new Router()
 
+// 解决跨域问题
+app.use(cors())
+
+app.use(bodyParser())
 // 加载模板引擎   （访问：localhost:3005/index.ejs）
 app.use(views(path.join(__dirname,'./view'),{
      map:{html:'ejs'}
@@ -17,6 +25,16 @@ app.use(views(path.join(__dirname,'./view'),{
 
 // 访问图片，直接通过 localhost:3005/hy.jpg 就可以访问到
 app.use(KoaStatic(path.join(__dirname,'./static')))
+
+// 利用jwt进行鉴权处理（登录和注册不用被鉴权）
+app.use(jwt.unless({
+  path:[/\/register/,/\/login/]
+}))
+
+// 按照功能的不同，将路由进行模块化处理
+router.use('/login',require('./route/loginRegister/login.js'))
+router.use('/register',require('./route/loginRegister/register.js'))
+
 
 
 // 处理一下图形验证码的路由
@@ -41,17 +59,18 @@ router.get('/code',async (ctx,next)=>{
 })
 
 
-// app.use(async (ctx)=>{
-//    const title='hello KOA2'
-//    await ctx.render('index',{
-//        title
-//    })
-// })
-
-
 
 app.use(router.routes())
 app.use(router.allowedMethods())
+
+// 连接数据库
+//  mongoose.connect('mongodb://localhost:27016/koaBlog',{useNewUrlParser: true },function(err,data){
+//     if(err){
+//       console.log('数据库报错:',err)
+//     }else{
+//       console.log('数据库连接成功')
+//     }
+//  })
 
 app.listen('3005',function(){
     console.log(' 3005 is running')
