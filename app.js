49baab4482860836e11aaf9492a184497d7dd660
xiaojiujiu <3507440900@qq.com>
2020-08-04 14:08:20 +0800

    更新入口文件3.0

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const views=require('koa-views')
 const cors=require('koa2-cors')
 const svgCaptcha=require('svg-captcha')
 const path=require('path')
-const {sign}=require('jsonwebtoken')
+const {sign,verify}=require('jsonwebtoken')
 const secret='demo'
-const jwt=require('koa-jwt')({secret})
+// 进行路由鉴权的(只需要在路由上加上Need_Token，即可实现权限路由)
+const jwt=require('koa-jwt')
+//const Need_Token=jwt({secret})
+const {checkToken,setToken,verToken}=require('./util/index')
 const mongoose=require('mongoose')
 
 const app=new Koa()
@@ -17,7 +20,9 @@ const router=new Router()
 // 解决跨域问题
 app.use(cors())
 
+// 解析post路由携带的数据
 app.use(bodyParser())
+
 // 加载模板引擎   （访问：localhost:3005/index.ejs）
 app.use(views(path.join(__dirname,'./view'),{
      map:{html:'ejs'}
@@ -26,51 +31,68 @@ app.use(views(path.join(__dirname,'./view'),{
 // 访问图片，直接通过 localhost:3005/hy.jpg 就可以访问到
 app.use(KoaStatic(path.join(__dirname,'./static')))
 
-// 利用jwt进行鉴权处理（登录和注册不用被鉴权）
-app.use(jwt.unless({
-  path:[/\/register/,/\/login/]
-}))
+app.use(async (ctx,next)=>{
+  var token=ctx.request.headers.authorization
+  if(token==undefined){
+    await next()
+  }else{
+    verToken(token).then((data)=>{
+      ctx.state={data} 
+    })
+    await next()
+  }
+})
 
-// 按照功能的不同，将路由进行模块化处理
-router.use('/login',require('./route/loginRegister/login.js'))
-router.use('/register',require('./route/loginRegister/register.js'))
-
-
-
-// 处理一下图形验证码的路由
-router.get('/code',async (ctx,next)=>{
-  const cap=svgCaptcha.create({
-      size:4,         // 验证码的长度
-      width:160,
-      height:50,
-      fontSize:50,
-      ignoreChars:'0oOliLI',   // 验证码的字符排除0oOliLI
-      noise:2,                 // 干扰线条数量
-      color:true,
-      background:'#eee'
-  })
-  let text=cap.text.toLowerCase()  // 忽略大小写
-  //ctx.set('Content-Type','image/svg+xml')
-  ctx.response.type='html'
-  ctx.response.body=`<a href="javascript: window.location.reload();">${cap.data}</a>`
-  console.log(cap.text)       // 图形验证码的结果
-//   console.log(ctx.request.body)
-//   console.log(ctx.request.method)
+// 验证token是否过期
+app.use(async (ctx,next)=>{
+   console.log('222')
+   return next().catch((err)=>{
+      if(err){
+          ctx.status=401          
+          ctx.response.body={
+             status:401,
+             message:'登录过期，请重新登录'
+          }
+      }
+    })
 })
 
 
+// 利用jwt进行鉴权处理（登录和注册不用被鉴权）
+app.use(jwt({secret}).unless({
+  path:[/\/register/,/\/login/]
+}))
+
+
+// 为所有的请求设置请求头Authorization(有些路由需要相关权限才能访问，这个权限就是token)
+/*app.use(async (ctx,next)=>{
+  let params=Object.assign({},ctx.request.query,ctx.request.body)
+  ctx.request.header={'authorization':"Bearer "+(params.token || '')}
+  await next()
+})*/
+
+// 检验token的有效性
+/*app.use(async (ctx,next)=>{
+  checkToken()(ctx,next)
+})*/
+
+// 按照功能的不同，将路由进行模块化处理
+// 登录和注册不用进行token的验证
+router.use('/login',require('./route/loginRegister/login.js'))
+router.use('/register',require('./route/loginRegister/register.js'))
+router.use('/api',require('./route/api/index'))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
 
 // 连接数据库
-//  mongoose.connect('mongodb://localhost:27016/koaBlog',{useNewUrlParser: true },function(err,data){
-//     if(err){
-//       console.log('数据库报错:',err)
-//     }else{
-//       console.log('数据库连接成功')
-//     }
-//  })
+mongoose.connect('mongodb://localhost:27016/koaBlog',{useNewUrlParser: true },function(err,data){
+    if(err){
+      console.log('数据库报错:',err)
+    }else{
+      console.log('数据库连接成功')
+    }
+})
 
 app.listen('3005',function(){
     console.log(' 3005 is running')
