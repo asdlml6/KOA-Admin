892f22e7f3227401ec18a8cbd95eb2d67d0359aa
xiaojiujiu <3507440900@qq.com>
2020-08-04 14:04:59 +0800

    工具模块2.0

--- util/index.js
+++ util/index.js
@@ -1,4 +1,6 @@
 const svgCaptcha=require('svg-captcha')
+const jwt=require('jsonwebtoken')
+const secret='demo'
 
 // 生成验证码
  const getSvgCaptcha=function(){
@@ -11,4 +13,60 @@ const svgCaptcha=require('svg-captcha')
    })
    return captcha 
 }
-module.exports={getSvgCaptcha}
+
+// 检查token的有效性（测试用例1,这个用例没用上）
+const checkToken=function(){
+  const checksToken = async function(ctx,next){
+    let token=ctx.request.headers["authorization"]
+    console.log(token)
+    if(token){   // 如果存在token就解析
+      const tokenItem=jwt.verify(token,secret,function(err,decode){
+         if(err){
+          ctx.response.body=err
+          return 
+         }
+      })    // 解析token
+      console.log('token',tokenItem)
+      const {time,timeout}=tokenItem              // 将token的创建时间和过期时间解构出来
+      let data=new Date().getTime()
+      if(data-time<=timeout){                     // 如果当前的时间-创建时间<=过期时间，说明token没有过期
+        await next()
+      }else{
+        ctx.response.body={
+          code:2,
+          message:'账号已经过期，请重新登录'
+        }
+      }
+    }
+  }
+  return checksToken
+}
+
+// 生成token（测试用例2）
+const setToken=function(username){
+   return new Promise((resolve,reject)=>{
+     const token=jwt.sign({
+       username,
+       timeout:Date.now()
+     },secret,{
+       expiresIn:'0.05h'
+     })
+     resolve(token)
+   }).catch((err)=>{
+     console.log('setToken err:',err)
+   })
+}
+
+// 解析token（并验证token是否有效）
+const verToken=function(token){
+  return new Promise((resolve,reject)=>{
+    var userInfo=jwt.verify(token.split(' ')[1],secret,{
+      complete:true
+    })
+    resolve(userInfo)
+  }).catch((err)=>{
+     console.log('verToken err:',err)
+  })
+}
+
+module.exports={getSvgCaptcha,checkToken,setToken,verToken}
